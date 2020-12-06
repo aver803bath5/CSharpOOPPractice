b90a28d0f5bf8328cee9fea05eb7bcbd70271ce5
chien-rei <aver803bath261@gmail.com>
2020-12-07 07:23:50 +0800

    Add new test

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -20,6 +20,15 @@ namespace OOP
             savings.MakeWithdrawal(250, DateTime.Now, "Needed to pay monthly bills");
             savings.PerformMonthEndTransactions();
             Console.WriteLine(savings.GetAccountHistory());
+
+            var lineOfCredit = new LineOfCreditAccount("line of credit", 0, 2000);
+            // How much is too much to borrow?
+            lineOfCredit.MakeWithdrawal(1000m, DateTime.Now, "Take out monthly advance");
+            lineOfCredit.MakeDeposit(50m, DateTime.Now, "Pay back small amount");
+            lineOfCredit.MakeWithdrawal(5000m, DateTime.Now, "Emergency funds for repairs");
+            lineOfCredit.MakeDeposit(150m, DateTime.Now, "Partial restoration on repairs");
+            lineOfCredit.PerformMonthEndTransactions();
+            Console.WriteLine(lineOfCredit.GetAccountHistory());
         }
     }
 }
